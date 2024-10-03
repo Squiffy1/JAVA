f62f383d81463e779834aab97182cc0c4025ade3
Squiffy1 <167431344+Squiffy1@users.noreply.github.com>
2024-10-03 17:30:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg3.pkg1hour;
+
+/**
+ *
+ * @author caleb
+ */
+public class Main {
+    //create object for tyhe class
+     int x,y,z;
+     String name;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main h=new Main();
+        Main h1=new Main();
+        Main h2=new Main();
+        h.x=5;
+        System.out.println("The valu of x is " +h.x);
+        h2.name="Joy";
+        h.y=15;
+        h.z=h.y+h.x;
+        System.out.println("The value of z is h.z"+h.z);
+               
+        // TODO code application logic here
+    }
+    
+}
